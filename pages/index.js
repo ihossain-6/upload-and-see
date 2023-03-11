3e5441e5e5bef4b600a10090e0e8abbaef9787e8
ihossain-6 <ismumhossain@gmail.com>
2023-03-11 12:50:35 +0600

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,51 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
+import { useStorageUpload } from "@thirdweb-dev/react";
+import React, { useState } from "react";
+import dynamic from 'next/dynamic';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function Home() {
+  const [file, setFile] = useState();
+  const [url, setUrl] = useState("");
+  const { mutateAsync: upload } = useStorageUpload();
+
+  const uploadToIpfs = async () => {
+    const [uploadUrl] = await upload({
+      data: [file],
+      options: { uploadWithGatewayUrl: true, uploadWithoutDirectory: true },
+    });
+    console.log({uploadUrl});
+    setUrl(uploadUrl);
+  };
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <div className="d-flex justify-content-center align-items-center flex-column">
+            <input
+              type="file"
+              className="mb-3"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <button className="btn btn-primary mb-3" onClick={uploadToIpfs}>
+              Upload
+            </button>
+            {url && (
+              <div className="react-player-wrapper">
+                <ReactPlayer
+                  url={url}
+                  className="react-player"
+                  width="100%"
+                  height="100%"
+                  controls={true}
+                />
+              </div>
+            )}
+          </div>
         </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
